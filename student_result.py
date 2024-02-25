be5965c426849068d30a5aeeb1c12de263a8da43
tolaoguntunde <tolaoguntunde@gmail.com>
2024-02-25 18:59:10 -0500

    upload initial files

--- student_result.py
+++ student_result.py
@@ -21,10 +21,15 @@ for val in df.values:
     student_grades[student_id]= [name,math,english, physics,chemistry]
 print()
 
+#request student id to pull record
 student_response = input("Please type your student id(e.g s****): ")
+#request student email to send the result
+student_email = input("Please type student email address: ")
+student_email = student_email.casefold() #this will convert email address to lowercase
 student_response = student_response.casefold()
 print("\n --------- Student Record -----------\n")
 
+#print student result to cmd
 if student_response in student_grades.keys():
     print(' {0} grades are as follows:\n \
         \n Mathematics - {1} \
@@ -35,3 +40,14 @@ if student_response in student_grades.keys():
             student_grades[student_response][3],student_grades[student_response][4]))
   
     print("\n------------------------------------")
+
+else:
+    print('Student record not found, please check the student id and try again')
+
+
+#TO DO
+#Update code such that if wrong student id is entered, prompt user for another student id
+#Refactor to use function where possible
+#Import library that would alloe connection to email: Connect to gmail or other email account to send the result
+#Are we sending the result as an attachment or should it go in the body of the email
+
